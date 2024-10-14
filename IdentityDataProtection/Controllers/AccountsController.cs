6b9ee875727921ead5ff031b0cb3e10f125527d3
Ahmet <Ahmet@DESKTOP-6Q0TJG5>
2024-10-14 12:53:11 +0300

    Add project files.

---
+++ IdentityDataProtection/Controllers/AccountsController.cs
@@ -0,0 +1,44 @@
+﻿using IdentityDataProtection.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityDataProtection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountsController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+
+        }
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return Ok(new { Massage = "Kayıt Başarılı" });
+                }
+                return BadRequest(new { errors = result.Errors.Select(e => e.Description) });
+            }
+            return BadRequest(new { errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage) });
+        }
+    }
+
+}
---
+++ IdentityDataProtection/Data/IdentityProtectionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityDataProtection.Data
+{
+    public class IdentityProtectionDbContext : IdentityDbContext<IdentityUser>
+    {
+        public IdentityProtectionDbContext(DbContextOptions<IdentityProtectionDbContext> options) : base(options)
+        {
+        }
+    }
+}
